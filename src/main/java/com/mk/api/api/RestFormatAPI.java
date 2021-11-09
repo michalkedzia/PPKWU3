e91dca174845bfb176af43a5ab83c0530978941b
michalkedzia <kedziamichal0@gmail.com>
2021-11-09 12:34:02 +0100

    Addding ednpoints

---
+++ src/main/java/com/mk/api/api/RestFormatAPI.java
@@ -0,0 +1,33 @@
+package com.mk.api.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class RestFormatAPI {
+
+    @GetMapping("/text/{parameter}")
+    public String responseAsText(@PathVariable("parameter") String parameter) {
+        return "text " + parameter;
+    }
+
+    @GetMapping("/json/{parameter}")
+    public String responseAsJSON(@PathVariable("parameter") String parameter) {
+        return "text " + parameter;
+    }
+
+    @GetMapping("/xml/{parameter}")
+    public String responseAsXML(@PathVariable("parameter") String parameter) {
+        return "text " + parameter;
+    }
+
+    @GetMapping("/csv/{parameter}")
+    public String responseAsCSV(@PathVariable("parameter") String parameter) {
+        return "text " + parameter;
+    }
+
+
+}
